ae36da54db3399fd8789bbd7dbc9a7900c37ab11
Indunil Moremada <imoremada@gmail.com>
2022-02-28 16:44:56 +0800

    initial commit with project structure

---
+++ bank-service/src/test/java/com/tpp/bank_service/BankAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpp.bank_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
