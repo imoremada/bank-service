c92ee9cf85a93954da96a1f3226d52590beb31ba
Indunil Moremada <imoremada@gmail.com>
2022-03-06 21:40:03 +0800

    DateTimeUtil has been updated to check last day of month

--- bank-service/src/main/java/com/tpp/bs/utility/DateTimeUtil.java
+++ bank-service/src/main/java/com/tpp/bs/utility/DateTimeUtil.java
@@ -1,6 +1,7 @@
 package com.tpp.bs.utility;
 
 import java.time.LocalDate;
+import java.time.OffsetDateTime;
 
 import static java.time.temporal.ChronoUnit.DAYS;
 
@@ -9,5 +10,11 @@ public class DateTimeUtil {
         LocalDate firstDayOfMonth = date.withDayOfMonth(1);
         LocalDate lastDayOfMonth = date.withDayOfMonth(date.getMonth().length(date.isLeapYear()));
         return (int) DAYS.between(firstDayOfMonth, lastDayOfMonth) + 1;
+    }
+
+    public static boolean isLastDayOfMonth(OffsetDateTime currentDate) {
+        LocalDate monthStartDate = LocalDate.of(currentDate.getYear(), currentDate.getMonth(), 1);
+        LocalDate monthEndDate = monthStartDate.plusDays(monthStartDate.lengthOfMonth() - 1);
+        return currentDate.toLocalDate().compareTo(monthEndDate) == 0;
     }
 }
--- bank-service/src/test/java/com/tpp/bs/utility/DateTimeUtilTest.java
+++ bank-service/src/test/java/com/tpp/bs/utility/DateTimeUtilTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.time.LocalDate;
+import java.time.*;
 
 @ExtendWith(MockitoExtension.class)
 class DateTimeUtilTest {
@@ -20,5 +20,33 @@ class DateTimeUtilTest {
 
         //Then
         Assertions.assertEquals(31,daysInMonth);
+    }
+
+    @Test
+    public void testShouldReturnFalseIfDateIsNotTheLastDayOfTheMonth(){
+        //Given
+        LocalDate localDate = LocalDate.of(2022, 3, 30);
+        LocalDateTime localDateTime = LocalDateTime.of(localDate, LocalTime.of(1, 5));
+        OffsetDateTime offsetDateTime = OffsetDateTime.of(localDateTime, ZoneOffset.UTC);
+
+        //When
+        boolean lastDayOfMonth = DateTimeUtil.isLastDayOfMonth(offsetDateTime);
+
+        //Then
+        Assertions.assertFalse(lastDayOfMonth);
+    }
+
+    @Test
+    public void testShouldReturnTrueForLastDayOfMonth(){
+        //Given
+        LocalDate localDate = LocalDate.of(2022, 3, 31);
+        LocalDateTime localDateTime = LocalDateTime.of(localDate, LocalTime.of(1, 5));
+        OffsetDateTime offsetDateTime = OffsetDateTime.of(localDateTime, ZoneOffset.UTC);
+
+        //When
+        boolean lastDayOfMonth = DateTimeUtil.isLastDayOfMonth(offsetDateTime);
+
+        //Then
+        Assertions.assertTrue(lastDayOfMonth);
     }
 }
